4968e49c0f3a6c145032b0a3d23e7e0193930338
Jasper Smet <josbeir@gmail.com>
2025-10-03 17:58:23 +0200

    Improve indents

--- languages/latte/indents.scm
+++ languages/latte/indents.scm
@@ -1,20 +1,20 @@
 ; Indent after opening tags
-(start_tag) @indent
+(start_tag) @indent.begin
 
 ; Dedent closing tags
-(end_tag) @outdent
+(end_tag) @indent.end
 
 ; Dedent self-closing tags
 (self_closing_tag) @outdent
 
 ; Indent Latte named blocks
-(block) @indent
+(block) @start
 
 ; Indent Latte control blocks
-(if_block) @indent
-(loop_block) @indent
-(switch_block) @indent
-(capture_tag) @indent
+(if_block) @start
+(loop_block) @start
+(switch_block) @start
+(capture_tag) @start
 
 ; Dedent else and elseif - these reset indentation within their parent blocks
 (else_block) @outdent
